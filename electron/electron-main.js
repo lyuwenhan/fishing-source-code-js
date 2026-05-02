28e628e4c315f42f26780be6906267bff25dd680
lyuwenhan <81000228+lyuwenhan@users.noreply.github.com>
2026-05-02 09:29:17 -0400

    Test workflow

---
+++ electron/electron-main.js
@@ -0,0 +1,36 @@
+import {
+	app,
+	BrowserWindow
+} from "electron";
+import path from "node:path";
+import {
+	fileURLToPath
+} from "node:url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1200,
+		height: 800,
+		icon: path.join(__dirname, "icons", "icon.ico"),
+		webPreferences: {
+			contextIsolation: true,
+			nodeIntegration: false
+		}
+	});
+	win.loadFile(path.join(__dirname, "index.html"))
+}
+app.whenReady().then(() => {
+	createWindow();
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow()
+		}
+	})
+});
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit()
+	}
+});
